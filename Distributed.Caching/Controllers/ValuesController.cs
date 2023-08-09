a8eae972fb778c723e0cfbe4e1b5c33167d184d6
Emirhan Öztürk <68188573+emirhanozturk@users.noreply.github.com>
2023-08-09 13:32:52 +0300

    Add project files.

---
+++ Distributed.Caching/Controllers/ValuesController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+using System.Text;
+
+namespace Distributed.Caching.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        IDistributedCache _distributedCache;
+
+        public ValuesController(IDistributedCache distributedCache)
+        {
+            _distributedCache = distributedCache;
+        }
+
+        [HttpGet("Set")]
+        public async Task<IActionResult> Set(string cachedValue,string cachedValueBinary)
+        {
+            await _distributedCache.SetStringAsync("redisCachedValue", cachedValue, options: new()
+            {
+                AbsoluteExpiration = DateTime.Now.AddSeconds(30),
+                SlidingExpiration = TimeSpan.FromSeconds(5)
+            });
+            await _distributedCache.SetAsync("redisCachedBinaryValue", Encoding.UTF8.GetBytes(cachedValueBinary), options: new()
+            {
+                AbsoluteExpiration = DateTime.Now.AddSeconds(30),
+                SlidingExpiration = TimeSpan.FromSeconds(5)
+            });
+            return Ok();
+        }
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var cachedValue  = await _distributedCache.GetStringAsync("redisCachedValue");
+            var cachedValueBinary = await _distributedCache.GetAsync("redisCachedBinaryValue");
+            var cachedValue2 = Encoding.UTF8.GetString(cachedValueBinary);
+            return Ok(new
+            {
+                cachedValue,
+                cachedValue2
+            });
+        }
+    }
+}
---
+++ InMemory.Caching/Controllers/ValuesController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace InMemory.Caching.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        public IMemoryCache _memoryCache;
+
+        public ValuesController(IMemoryCache memoryCache)
+        {
+            _memoryCache = memoryCache;
+        }
+
+        [HttpGet("set/{cachedValue}")]
+        public void SetCache(string cachedValue)
+        {
+            _memoryCache.Set("cached", cachedValue);
+        }
+
+        [HttpGet]
+        public string GetCache()
+        {
+            if (_memoryCache.TryGetValue<string>("cached", out string name))
+            {
+                return name;
+            }
+            return "";
+            //  return _memoryCache.Get<string>("cached");
+        }
+
+        //Absolute time: Cache'deki datanın ne kadar tutulacağına dair net ömrünün belirtilmesidir. Belirtilen süre sona erdiğinde cache temizlenir.
+        //Sliding Time:  Cache'deki datanın memoryde belirtilen süre bpyunda tutulmasını belirtir. Belirtilen süre peridyoru içerisinde cache'e yapılan erişim neticesinde de datanın ömrü bir o kadar uzatılacaktır. Belirlenen süre içerisinde dataya erişilmezse cache temizlenir.
+        //Absolute time ve Sliding Time aynı anda kullanılabilir.
+
+        [HttpGet("setDateValueCache")]
+        public void SetDateValueCache()
+        {
+            _memoryCache.Set<DateTime>("date", DateTime.Now,options: new()
+            {
+                AbsoluteExpiration= DateTime.Now.AddSeconds(30),
+                SlidingExpiration= TimeSpan.FromSeconds(5)
+            });
+        }
+
+        [HttpGet]
+        public DateTime GetDateValueCache() 
+        {
+            return _memoryCache.Get<DateTime>("date");
+        }
+
+    }
+}
